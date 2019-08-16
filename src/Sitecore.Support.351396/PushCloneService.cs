0d2e18afe210ad115103990d157c56d974bf95cc
Ihor <iro@sitecore.net>
2019-08-16 13:31:21 +0300

    Initial

---
+++ src/Sitecore.Support.351396/PushCloneService.cs
@@ -0,0 +1,20 @@
+﻿namespace Sitecore.Support.XA.Foundation.Multisite.Services
+{
+    using Sitecore.Data.Comparers;
+    using Sitecore.Data.Items;
+    using Sitecore.XA.Foundation.Multisite.Services;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PushCloneService : Sitecore.XA.Foundation.Multisite.Services.PushCloneService
+    {
+
+        public PushCloneService(IPushCloneCoordinatorService pushCloneCoordinatorService) : base(pushCloneCoordinatorService)
+        { }
+
+        protected override IEnumerable<Item> GetCloneItem(Item item)
+        {
+            return item.GetClones().Distinct(new ItemIdComparer());
+        }
+    }
+}
